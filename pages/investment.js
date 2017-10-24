997f6c1b19d4530d8ab92d0ca9b3eb3d1b557be2
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-10-24 21:06:31 -0200

    Show gain from last month

--- pages/investment.js
+++ pages/investment.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Router from 'next/router'
 import PropTypes from 'prop-types'
+import {
+  green700 as greenColor,
+  red800 as redColor
+} from 'material-ui/styles/colors'
 import { Card, CardActions, CardHeader, CardMedia } from 'material-ui/Card'
 import {
   Table,
@@ -13,15 +17,26 @@ import {
 import FlatButton from 'material-ui/FlatButton'
 import Layout from '../components/MyLayout.js'
 import { getInvestment, getIncomes, getToken } from '../components/Api'
+import { formatMoney } from '../utils/number'
 
 class Index extends React.Component {
   static async getInitialProps ({ query, req }) {
     const investment = await getInvestment(getToken(req), query.id)
     const incomes = await getIncomes(getToken(req), query.id)
+    var lastIncomeValue = 0
 
     return {
       investment: investment.data,
       incomes: incomes.data
+        .reverse()
+        .map(item => {
+          item.lastIncomeValue =
+            lastIncomeValue !== 0 ? lastIncomeValue : item.value
+          lastIncomeValue = item.value
+          item.gain = item.value - item.lastIncomeValue
+          return item
+        })
+        .reverse()
     }
   }
 
@@ -50,6 +65,7 @@ class Index extends React.Component {
                   <TableHeaderColumn>Data</TableHeaderColumn>
                   <TableHeaderColumn>Quantidade</TableHeaderColumn>
                   <TableHeaderColumn>Valor</TableHeaderColumn>
+                  <TableHeaderColumn>Ganho/Perda</TableHeaderColumn>
                 </TableRow>
               </TableHeader>
               <TableBody displayRowCheckbox={false} showRowHover stripedRows>
@@ -57,7 +73,16 @@ class Index extends React.Component {
                   <TableRow key={item._id}>
                     <TableRowColumn>{item.date}</TableRowColumn>
                     <TableRowColumn>{item.quantity}</TableRowColumn>
-                    <TableRowColumn>{item.value}</TableRowColumn>
+                    <TableRowColumn>
+                      {formatMoney(item.value, 2)}
+                    </TableRowColumn>
+                    <TableRowColumn
+                      style={{
+                        color: item.gain >= 0 ? greenColor : redColor
+                      }}
+                    >
+                      {formatMoney(item.gain, 2)}
+                    </TableRowColumn>
                   </TableRow>
                 ))}
               </TableBody>
@@ -73,7 +98,8 @@ class Index extends React.Component {
 }
 
 Index.propTypes = {
-  investment: PropTypes.element.isRequired
+  investment: PropTypes.element.isRequired,
+  incomes: PropTypes.array.isRequired
 }
 
 export default Index
