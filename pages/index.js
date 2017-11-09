ed97f15b48557b158dbf4d4a08effe68fa27600a
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-11-08 23:19:47 -0200

    Recharts and type pie chart

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Card, CardMedia, CardHeader } from 'material-ui/Card'
 import { Table, TableHeader, TableBody, TableRow, TableHeaderColumn, TableRowColumn } from 'material-ui/Table'
+import { ResponsiveContainer, PieChart, Pie, Cell, Legend, Tooltip } from 'recharts'
 import Layout from '../components/MyLayout.js'
 import { getInvestments, getIncomes, getToken } from '../components/Api'
 import { formatMoney } from '../utils/number'
+
+const COLORS = ['#4eb8ea', '#ac92ec', '#96c823', '#ef5a31', '#FFC107']
 
 class Index extends React.Component {
   static async getInitialProps ({ req }) {
@@ -23,23 +26,37 @@ class Index extends React.Component {
       }
 
       var type = investment.type
-      if (!investmentsByType[type]) investmentsByType[type] = []
-      investmentsByType[type].push(investment)
+      if (!investmentsByType[type]) {
+        investmentsByType[type] = {}
+        investmentsByType[type].investments = []
+        investmentsByType[type].value = 0
+      }
+      investmentsByType[type].investments.push(investment)
+      investmentsByType[type].value += investment.currentValue
     }
 
-    investmentsByType['Total'] = investments
+    var investmentsByTypeChartData = []
+    for (let type in investmentsByType) {
+      investmentsByTypeChartData.push({ name: type, value: investmentsByType[type].value })
+    }
+
+    investmentsByType['Total'] = {}
+    investmentsByType['Total'].investments = investments
+    investmentsByType['Total'].value = investments.reduce((acum, investment) => acum + investment.currentValue, 0)
 
     return {
       investments: investments,
-      investmentsByType: investmentsByType
+      investmentsByType: investmentsByType,
+      investmentsByTypeChartData: investmentsByTypeChartData
     }
   }
 
   render () {
-    const { investmentsByType } = this.props
+    const { investmentsByType, investmentsByTypeChartData } = this.props
+
     return (
       <Layout title="Dashboard">
-        <Card style={{ width: '50%' }}>
+        <Card style={{ width: '45%' }}>
           <CardHeader title="Totais por tipo" />
           <CardMedia>
             <Table fixedHeader selectable={false}>
@@ -55,11 +72,25 @@ class Index extends React.Component {
                   .map(type => (
                     <TableRow key={type}>
                       <TableRowColumn>{type}</TableRowColumn>
-                      <TableRowColumn>{formatMoney(investmentsByType[type].reduce((acum, investment) => acum + investment.currentValue, 0), 2)}</TableRowColumn>
+                      <TableRowColumn>{formatMoney(investmentsByType[type].value, 2)}</TableRowColumn>
                     </TableRow>
                   ))}
               </TableBody>
             </Table>
+          </CardMedia>
+        </Card>
+        <Card style={{ width: '45%' }}>
+          <CardHeader title="Totais por tipo" />
+          <CardMedia>
+            <ResponsiveContainer width="45%" height={400}>
+              <PieChart>
+                <Tooltip />
+                <Pie data={investmentsByTypeChartData} innerRadius={90} outerRadius={110} fill="#8884d8" paddingAngle={3}>
+                  {investmentsByTypeChartData.map((entry, index) => <Cell key={entry} fill={COLORS[index % COLORS.length]} />)}
+                </Pie>
+                <Legend iconType="cicle" />
+              </PieChart>
+            </ResponsiveContainer>
           </CardMedia>
         </Card>
       </Layout>
@@ -69,7 +100,8 @@ class Index extends React.Component {
 
 Index.propTypes = {
   investments: PropTypes.array.isRequired,
-  investmentsByType: PropTypes.element.isRequired
+  investmentsByType: PropTypes.element.isRequired,
+  investmentsByTypeChartData: PropTypes.array.isRequired
 }
 
 export default Index
