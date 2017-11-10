9db799a9c0a31eebd8aec4ac04ed4b184faf9dae
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-11-10 21:25:23 -0200

    Separate IncomesByMonth tables with react-grid

--- components/dashboard/IncomesByMonth.js
+++ components/dashboard/IncomesByMonth.js
@@ -2,9 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Card, CardMedia, CardHeader } from 'material-ui/Card'
 import { Table, TableHeader, TableBody, TableRow, TableHeaderColumn, TableRowColumn } from 'material-ui/Table'
+import { Container, Row, Col } from 'react-grid-system'
 import moment from 'moment'
 import { Month } from '../../components/Date'
-import { MoneyWithColor } from '../../components/Money'
+import { Money, MoneyWithColor } from '../../components/Money'
 
 const totalId = 'Total'
 
@@ -19,6 +20,7 @@ class IncomesByMonth extends React.Component {
     const { investments } = this.props
 
     var investmentsByMonth = {}
+    var totalValue = 0
     investments.forEach(investment => {
       investment.incomes.filter(income => moment(income.date).year() === 2017).forEach(income => {
         var monthData = investmentsByMonth[income.date]
@@ -44,6 +46,8 @@ class IncomesByMonth extends React.Component {
         monthData[totalId] = totalData
         investmentsByMonth[income.date] = monthData
       })
+
+      totalValue += investment.currentValue
     })
 
     Object.keys(investmentsByMonth).map(month => {
@@ -54,6 +58,7 @@ class IncomesByMonth extends React.Component {
       })
     })
 
+    this.totalValue = totalValue
     this.investmentsByMonth = investmentsByMonth
   }
 
@@ -65,38 +70,65 @@ class IncomesByMonth extends React.Component {
       <Card>
         <CardHeader title="Recebimentos por mês" />
         <CardMedia>
-          <Table selectable={false}>
-            <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-              <TableRow>
-                <TableHeaderColumn>Nome</TableHeaderColumn>
-                {Object.keys(investmentsByMonth).map(month => (
-                  <TableHeaderColumn key={month}>
-                    <Month date={month} />
-                  </TableHeaderColumn>
-                ))}
-              </TableRow>
-            </TableHeader>
-            <TableBody displayRowCheckbox={false} showRowHover stripedRows>
-              {investments.map(investment => (
-                <TableRow key={investment._id}>
-                  <TableRowColumn>{investment.name}</TableRowColumn>
-                  {Object.keys(investmentsByMonth).map(month => (
-                    <TableRowColumn key={month + investment._id}>
-                      <MoneyWithColor value={investmentsByMonth[month][investment._id].value} />
+          <Container>
+            <Row>
+              <Col lg={3}>
+                <Table selectable={false}>
+                  <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+                    <TableRow>
+                      <TableHeaderColumn>Nome</TableHeaderColumn>
+                      <TableHeaderColumn style={{ width: 100 }}>Valor</TableHeaderColumn>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody displayRowCheckbox={false} showRowHover stripedRows>
+                    {investments.map(investment => (
+                      <TableRow key={investment._id}>
+                        <TableRowColumn>{investment.name}</TableRowColumn>
+                        <TableRowColumn style={{ width: 100 }}>
+                          <Money value={investment.currentValue} />
+                        </TableRowColumn>
+                      </TableRow>
+                    ))}
+                    <TableRowColumn>Total</TableRowColumn>
+                    <TableRowColumn>
+                      <Money value={this.totalValue} />
                     </TableRowColumn>
-                  ))}
-                </TableRow>
-              ))}
-              <TableRow key={0}>
-                <TableRowColumn>Total</TableRowColumn>
-                {Object.keys(investmentsByMonth).map(month => (
-                  <TableRowColumn key={month + totalId}>
-                    <MoneyWithColor value={investmentsByMonth[month][totalId].value} />
-                  </TableRowColumn>
-                ))}
-              </TableRow>
-            </TableBody>
-          </Table>
+                  </TableBody>
+                </Table>
+              </Col>
+              <Col lg={9}>
+                <Table selectable={false}>
+                  <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+                    <TableRow>
+                      {Object.keys(investmentsByMonth).map(month => (
+                        <TableHeaderColumn key={month}>
+                          <Month date={month} />
+                        </TableHeaderColumn>
+                      ))}
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody displayRowCheckbox={false} showRowHover stripedRows>
+                    {investments.map(investment => (
+                      <TableRow key={investment._id}>
+                        {Object.keys(investmentsByMonth).map(month => (
+                          <TableRowColumn key={month + investment._id}>
+                            <MoneyWithColor value={investmentsByMonth[month][investment._id].value} />
+                          </TableRowColumn>
+                        ))}
+                      </TableRow>
+                    ))}
+                    <TableRow key={0}>
+                      {Object.keys(investmentsByMonth).map(month => (
+                        <TableRowColumn key={month + totalId}>
+                          <MoneyWithColor value={investmentsByMonth[month][totalId].value} />
+                        </TableRowColumn>
+                      ))}
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </Col>
+            </Row>
+          </Container>
         </CardMedia>
       </Card>
     )
